2dd98cc4c3675e19ebee82175d808a69383279dc
ryzen7800xt <beccaleb.13@gmail.com>
2026-08-12 08:43:42 +1000

    Add Item structure for product representation
    commit maxxing

--- main.go
+++ main.go
@@ -8,3 +8,14 @@ import (
 	"net/http"
 	"os"
 )
+
+type Item struct {
+		ID    string  `json:"id"`
+		Name  string  `json:"name"`
+		Price float64 `json:"price"`
+}
+
+	var dbpool  *pgxpool.Pool
+
+func main() {
+}
